1d8807983a639c19cac424674e0a6d74196d1bb2
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-25 16:23:30 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    users=User.all
+    render json: users, include: :items
+  
+  end
+
+  def show
+    user=User.find_by(params[:id])
+    render json: user,include: :items
+  end
 end
